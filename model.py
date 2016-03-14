119418990069bde674b39259c73d97b50d9cc24d
fenixlin <fenixlin@yandex.com>
2016-03-13 23:43:43 -0700

    Random forest classifier added

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 from sklearn.decomposition import TruncatedSVD
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import mean_squared_error, make_scorer
@@ -68,6 +68,57 @@ def random_forest_regression_(x_train, y_train, x_test):
     print(model.best_score_)
 
     y_pred = model.predict(x_test)
+    return y_predo
+
+def transform_labels_(y_pred):
+    labels=[1., 1.25, 1.33, 1.5, 1.67, 1.75, 2., 2.25, 2.33, 2.5, 2.67, 2.75, 3]
+    for i in range(len(y_pred)):
+        for j in range(len(labels)):
+            if y_pred[i]==labels[j]:
+                y_pred[i] = j
+                break
+    return y_pred
+
+def recover_labels_(y_pred):
+    labels=[1., 1.25, 1.33, 1.5, 1.67, 1.75, 2., 2.25, 2.33, 2.5, 2.67, 2.75, 3]
+    for i in range(len(y_pred)):
+        y_pred[i] = labels[int(y_pred[i])]
+    return y_pred
+
+def random_forest_classification_(x_train, y_train, x_test):
+    y_train = transform_labels_(y_train)
+    rfc = RandomForestClassifier(n_estimators = 500, n_jobs = -1, random_state = 2016, verbose = 1)
+    tfidf = TfidfVectorizer(ngram_range=(1, 1), stop_words='english')
+    tsvd = TruncatedSVD(n_components=10, random_state = 2016)
+    clf = pipeline.Pipeline([
+            ('union', FeatureUnion(
+                        transformer_list = [
+                            ('cst',  cust_regression_vals_()),
+                            ('txt1', pipeline.Pipeline([('s1', cust_txt_col_(key='search_term_fuzzy_match')), ('tfidf1', tfidf), ('tsvd1', tsvd)])),
+                            ('txt2', pipeline.Pipeline([('s2', cust_txt_col_(key='title')), ('tfidf2', tfidf), ('tsvd2', tsvd)])),
+                            ('txt3', pipeline.Pipeline([('s3', cust_txt_col_(key='description')), ('tfidf3', tfidf), ('tsvd3', tsvd)])),
+                            ('txt4', pipeline.Pipeline([('s4', cust_txt_col_(key='brand')), ('tfidf4', tfidf), ('tsvd4', tsvd)]))
+                            ],
+                        transformer_weights = {
+                            'cst': 1.0,
+                            'txt1': 0.5,
+                            'txt2': 0.25,
+                            'txt3': 0.0,
+                            'txt4': 0.5
+                            },
+                    #n_jobs = -1
+                    )),
+            ('rfc', rfc)])
+    param_grid = {'rfc__max_features': [10], 'rfc__max_depth': [20]}
+    model = grid_search.GridSearchCV(estimator = clf, param_grid = param_grid, n_jobs = 1, cv = 2, verbose = 20, scoring=RMSE)
+    model.fit(x_train, y_train)
+
+    print("Best parameters found by grid search:")
+    print(model.best_params_)
+    print("Best CV score:")
+    print(model.best_score_)
+
+    y_pred = recover_labels_(model.predict(x_test))
     return y_pred
 
 def xgboost_regression_(x_train, y_train, x_test):
@@ -102,7 +153,7 @@ subsample=1, colsample_bytree=1, colsample_bylevel=1, reg_alpha=0, reg_lambda=1,
     print(model.best_params_)
     print("Best CV score:")
     print(model.best_score_)
-    print(model.best_score_ + 0.47003199274) # why original script do that?
+    #print(model.best_score_ + 0.47003199274) # why original script do that?
 
     y_pred = model.predict(x_test)
     for i in range(len(y_pred)):
@@ -114,6 +165,7 @@ subsample=1, colsample_bytree=1, colsample_bylevel=1, reg_alpha=0, reg_lambda=1,
 
 ModelDict = {
     'rfr': random_forest_regression_,
+    'rfc': random_forest_classification_,
     'xgbr': xgboost_regression_,
 }
 
